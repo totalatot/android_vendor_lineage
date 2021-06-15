681042908bd693de521c586a4f6d07e19f242be7
TotalChen <totalatot@gmail.com>
2021-06-16 02:51:43 +0800

    Enable ADB by default
    Change-Id: I28a944d0365e583de7018dda6b1f6e2e8b20cc24

--- config/common.mk
+++ config/common.mk
@@ -17,16 +17,15 @@ endif
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.setupwizard.mode=DISABLED
 
-ifeq ($(TARGET_BUILD_VARIANT),eng)
+#ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
-else
+#else
 # Enable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
-
+#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
-endif
+#endif
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
